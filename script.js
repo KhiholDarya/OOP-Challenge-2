8a58d044807f33c822899c229a352bf30ceb5bb5
Darya Khikhol <khikhol.darya@mail.ru>
2024-01-07 19:29:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+class Car {
+	constructor(make, speed) {
+		this.make = make;
+		this.speed = speed;
+	};
+	accelerate () {
+		this.speed += 10;
+		console.log(`${this.make} going at ${this.speed} km/h`);
+	};
+	brake () {
+		this.speed -= 5;
+		console.log(`${this.make} going at ${this.speed} km/h`);
+	};
+	get speedUS () {
+		console.log(`${this.make} going at ${Math.floor(this.speed / 1.6)} mi/h`);
+	};
+	set speedUS (curSpeed) {
+		console.log(`${this.make} going at ${curSpeed * 1.6} km/h`);
+	};
+};
+
+// DATA CAR 1: 'BMW' going at 120 km/h
+const bmw = new Car ('BMW', 120);
+// DATA CAR 2: 'Mercedes' going at 95 km/h
+const mercedes = new Car ('Mercedes', 95);
+// DATA CAR 3: 'Ford' going at 120 km/h
+const ford = new Car ('Ford', 120);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
+bmw.speedUS;
+bmw.speedUS = 75;
